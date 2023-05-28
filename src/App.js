a8daca9c09ace9654ab19e48f5b7fca13c84fc05
nouftalajmi <alajminouf94@gmail.com>
2023-05-28 19:06:14 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import image from "./assets/image/cookieshop.jpeg";
 
+// import { fragment } from "";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <h1 className="title"> welcome to cookies and beyond!</h1>
+        <h4>crumble cookies are the most ordered cookies</h4>
+        <img src={image} className="cookiesShop" alt="shop"></img>
+      </div>
+    </>
   );
 }
 
